5c420efc68c68fe295ae586cb5577e21ffcdee08
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-25 22:30:40 -0500

    Restructure double go-to

--- hangman.for
+++ hangman.for
@@ -43,8 +43,9 @@
        N = " "
        U = 0
        M=0 
-       IF (C .LT. 50) GO TO 100
-       WRITE (*,*) "You did all the words"; GO TO 999
+       IF (C .GE. 3) THEN
+           WRITE (*,*) "You did all the words"; GO TO 999
+       END IF
 100    Q=CEILING(RAND()*50)
        IF (U(Q) - 1) 120,100,120
 120    U(Q) = 1; C=C+1; T1=0
