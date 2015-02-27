714b650df5c868cdc01b6a458c32239e8534e08a
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-27 10:34:03 -0500

    Better variable name. p->image

--- hangman.for
+++ hangman.for
@@ -5,7 +5,7 @@
 !       
        program hangman 
        implicit none
-       character :: p(12,12)
+       character :: image(12,12)
        character :: guesses(26)
        character :: dashes(20), a*20, guess, b*20, ans 
        integer :: used(50)
@@ -36,14 +36,14 @@
 
        do counter=1,50
 ! assign ascii gallows 
-           p = " "
+           image = " "
            do i = 1,12
-               p(i,1) = "x"
+               image(i,1) = "x"
            end do
            do j = 1,7
-               p(1,j) = "x"
+               image(1,j) = "x"
            end do
-           p(2,7) = "x"
+           image(2,7) = "x"
 
 ! initialize other values
            dashes = "-"
@@ -123,59 +123,59 @@
                select case (mistakes)
                    case (1)
                        write (*,*) "first we draw a head."
-                       p(3,6) = "-"
-                       p(3,7) = "-"
-                       p(3,8) = "-"
-                       p(4,5) = "(" 
-                       p(4,6) = "."
-                       p(4,8) = "."
-                       p(4,9) = ")"
-                       p(5,6) = "-"
-                       p(5,7) = "-" 
-                       p(5,8) = "-"
+                       image(3,6) = "-"
+                       image(3,7) = "-"
+                       image(3,8) = "-"
+                       image(4,5) = "(" 
+                       image(4,6) = "."
+                       image(4,8) = "."
+                       image(4,9) = ")"
+                       image(5,6) = "-"
+                       image(5,7) = "-" 
+                       image(5,8) = "-"
                    case (2)
                        write (*,*) "now we draw a body."
                        do i = 6,9
-                           p(i,7) = "x" 
+                           image(i,7) = "x" 
                        end do
                    case (3)
                        write (*,*) "next we draw an arm."
                        do i = 4,7
-                           p(i,i-1) = "\" 
+                           image(i,i-1) = "\" 
                        end do
                    case (4)
                        write (*,*) "this time it's the other arm."
-                       p(4,11) = "/"
-                       p(5,10) = "/"
-                       p(6,9) = "/"
-                       p(7,8) = "/" 
+                       image(4,11) = "/"
+                       image(5,10) = "/"
+                       image(6,9) = "/"
+                       image(7,8) = "/" 
                    case (5)
                        write (*,*) "now, let's draw the right leg."
-                       p(10,6) = "/"
-                       p(11,5) = "/"
+                       image(10,6) = "/"
+                       image(11,5) = "/"
                    case (6)
                        write (*,*) "this time we draw the left leg."
-                       p(10,8) = "\"
-                       p(11,9) = "\"
+                       image(10,8) = "\"
+                       image(11,9) = "\"
                    case (7)
                        write (*,*) "now we put up a hand."
-                       p(3,11) = "\"
+                       image(3,11) = "\"
                    case (8)
                        write (*,*) "next the other hand."
-                       p(3,3) = "/"
+                       image(3,3) = "/"
                    case (9)
                        write (*,*) "now we draw one foot."
-                       p(12,10) = "\"
-                       p(12,11) = "-"
+                       image(12,10) = "\"
+                       image(12,11) = "-"
                    case (10)
                        write (*,*) "here's the other foot"
                        write (*,*) "you're hung!!."
-                       p(12,3) = "-"
-                       p(12,4) = "/" 
+                       image(12,3) = "-"
+                       image(12,4) = "/" 
                end select
 
                do i = 1,12
-                   write (*,*) (p(i,j),j=1,12)
+                   write (*,*) (image(i,j),j=1,12)
                end do
            end do
            if (mistakes .eq. 10) then
