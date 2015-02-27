17a0ac2f8d03473c27de00080c2b58601f4e65e2
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-27 11:40:54 -0500

    Don't rely on cycle/exit. Explicitly show mistakes

--- hangman.f95
+++ hangman.f95
@@ -98,7 +98,68 @@ do counter=1,50
             end if
         end do
  
-        if (n_replaced .ne. 0) then
+        if (n_replaced .eq. 0) then
+            mistakes = mistakes + 1
+            write (*,*) "sorry, that letter isn't in the word."
+            select case (mistakes)
+                case (1)
+                    write (*,*) "first we draw a head."
+                    image(3,6) = "-"
+                    image(3,7) = "-"
+                    image(3,8) = "-"
+                    image(4,5) = "(" 
+                    image(4,6) = "."
+                    image(4,8) = "."
+                    image(4,9) = ")"
+                    image(5,6) = "-"
+                    image(5,7) = "-" 
+                    image(5,8) = "-"
+                case (2)
+                    write (*,*) "now we draw a body."
+                    do i = 6,9
+                        image(i,7) = "x" 
+                    end do
+                case (3)
+                    write (*,*) "next we draw an arm."
+                    do i = 4,7
+                        image(i,i-1) = "\" 
+                    end do
+                case (4)
+                    write (*,*) "this time it's the other arm."
+                    image(4,11) = "/"
+                    image(5,10) = "/"
+                    image(6,9) = "/"
+                    image(7,8) = "/" 
+                case (5)
+                    write (*,*) "now, let's draw the right leg."
+                    image(10,6) = "/"
+                    image(11,5) = "/"
+                case (6)
+                    write (*,*) "this time we draw the left leg."
+                    image(10,8) = "\"
+                    image(11,9) = "\"
+                case (7)
+                    write (*,*) "now we put up a hand."
+                    image(3,11) = "\"
+                case (8)
+                    write (*,*) "next the other hand."
+                    image(3,3) = "/"
+                case (9)
+                    write (*,*) "now we draw one foot."
+                    image(12,10) = "\"
+                    image(12,11) = "-"
+                case (10)
+                    write (*,*) "here's the other foot"
+                    write (*,*) "you're hung!!."
+                    image(12,3) = "-"
+                    image(12,4) = "/" 
+            end select
+     
+            ! draw the ascii diagram
+            do i = 1,12
+                write (*,*) (image(i,j),j=1,12)
+            end do
+        else
             if (any(dashes(1:length) .eq. '-')) then
                 write (*,*) dashes(1:length)
                 write (*,*) "what is your guess for the word? "
@@ -114,67 +175,8 @@ do counter=1,50
                 exit
             end if
         end if
- 
-        mistakes = mistakes+1
-        write (*,*) "sorry, that letter isn't in the word."
-        select case (mistakes)
-            case (1)
-                write (*,*) "first we draw a head."
-                image(3,6) = "-"
-                image(3,7) = "-"
-                image(3,8) = "-"
-                image(4,5) = "(" 
-                image(4,6) = "."
-                image(4,8) = "."
-                image(4,9) = ")"
-                image(5,6) = "-"
-                image(5,7) = "-" 
-                image(5,8) = "-"
-            case (2)
-                write (*,*) "now we draw a body."
-                do i = 6,9
-                    image(i,7) = "x" 
-                end do
-            case (3)
-                write (*,*) "next we draw an arm."
-                do i = 4,7
-                    image(i,i-1) = "\" 
-                end do
-            case (4)
-                write (*,*) "this time it's the other arm."
-                image(4,11) = "/"
-                image(5,10) = "/"
-                image(6,9) = "/"
-                image(7,8) = "/" 
-            case (5)
-                write (*,*) "now, let's draw the right leg."
-                image(10,6) = "/"
-                image(11,5) = "/"
-            case (6)
-                write (*,*) "this time we draw the left leg."
-                image(10,8) = "\"
-                image(11,9) = "\"
-            case (7)
-                write (*,*) "now we put up a hand."
-                image(3,11) = "\"
-            case (8)
-                write (*,*) "next the other hand."
-                image(3,3) = "/"
-            case (9)
-                write (*,*) "now we draw one foot."
-                image(12,10) = "\"
-                image(12,11) = "-"
-            case (10)
-                write (*,*) "here's the other foot"
-                write (*,*) "you're hung!!."
-                image(12,3) = "-"
-                image(12,4) = "/" 
-        end select
- 
-        do i = 1,12
-            write (*,*) (image(i,j),j=1,12)
-        end do
     end do
+
     if (mistakes .eq. 10) then
         write (*,*) "sorry, you loose. the word was ", word
         write (*,*) "you missed that one."
