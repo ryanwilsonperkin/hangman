1f26817c64999fee3752e2563f3175aa8e4d42db
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2015-02-27 22:36:12 -0500

    Get random word from dictionary.

--- hangman.adb
+++ hangman.adb
@@ -1,5 +1,11 @@
 with ada.Text_IO; use Ada.Text_IO;
+with ada.Numerics.Discrete_Random;
 procedure Hangman is
+	type Word_Range is range 1..50;
+	package Rand_Index is new ada.Numerics.Discrete_Random(Word_Range);
+	use Rand_Index;
+	gen : Rand_Index.Generator;
+
 	image : array(1..12) of string(1..12);
 	guesses : string(1..26);
 	letter_guess : character;
@@ -17,13 +23,13 @@ procedure Hangman is
 	n_guesses : integer;
 	n_mistakes : integer;
 
-	used : array(1..50) of Boolean := (1..50 => false);
-	dict_index : integer;
-	dictionary_lengths : array(1..50) of integer := (
+	used : array(Word_Range) of Boolean := (Word_Range => false);
+	dict_index : Word_Range;
+	dictionary_lengths : array(Word_Range) of integer := (
 		3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5,
 		6, 6, 6, 6, 6, 6, 7, 7, 7, 6, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9,
 		9, 10, 11, 11, 10, 11, 19, 13);
-	dictionary : array(1..50) of string(1..20) := (
+	dictionary : array(Word_Range) of string(1..20) := (
 		"gum                 ", "sin                 ", "for                 ",
 		"cry                 ", "lug                 ", "bye                 ",
 		"fly                 ", "ugly                ", "each                ",
@@ -58,6 +64,15 @@ begin
 		guesses := (1..26 => ' ');
 		n_mistakes := 0;
 		n_guesses := 0;
-	end loop;
 
+		-- Get random word from dictionary
+		loop
+			dict_index := Random(gen);
+			exit when used(dict_index) = false;
+		end loop;
+		used(dict_index) := true;
+		word := dictionary(dict_index);
+		word_length := dictionary_lengths(dict_index);
+		hidden_length := word_length;
+	end loop;
 end Hangman;
